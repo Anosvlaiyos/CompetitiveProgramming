6f3ec334cff0ce66cf1ad71a45c4cbea600758dc
Kevin Wan <42050404+Lyestria@users.noreply.github.com>
2020-06-19 20:53:59 -0400

    Update convexhull.h

--- convexhull.h
+++ convexhull.h
@@ -1,21 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+typedef pair<ll,ll>pll;
+ll dot(pll a,pll b){return a.first*b.first+a.second*b.second;}
+ll cross(pll a,pll b){return a.first*b.second-a.second-b.first;}
+bool ccw(pll a,pll b,pll c){return cross({b.first-a.first,b.second-a.second},{c.first-b.first,c.second-b.second})>0;}
 vector<pll> cvxhull(vector<pll> p){
+	int n=p.size();
 	sort(p.begin(),p.end());
-	cvx.push_back(p[0]);
-	cvx.push_back(p[1]);
-	for (i = 2; i<n; i++) {
-		while (!ccw(cvx[cvx.size() - 2], cvx[cvx.size() - 1], p[i])) {
-			cvx.pop_back(); if (cvx.size() == 1)break;
-		}
+	vector<pll>cvx;
+	for (i = 0; i<n; i++) {
+		while(cvx.size()>=2&&!ccw(cvx[cvx.size()-2],cvx.back(),p[i]))cvx.pop_back();
 		cvx.push_back(p[i]);
 	}
-	int idek = cvx.size();
-	cvx.push_back(p[n - 2]);
-	for (i = n - 3; i >= 0; i--) {
-		while (!ccw(cvx[cvx.size() - 2], cvx[cvx.size() - 1], p[i])) {
-			cvx.pop_back(); if (cvx.size() == idek)break;
-		}
+	for (i = n - 2; i >= 0; i--) {
+		while(cvx.size()>=2&&!ccw(cvx[cvx.size()-2],cvx.back(),p[i]))cvx.pop_back();
 		cvx.push_back(p[i]);
 	}
 	cvx.pop_back();

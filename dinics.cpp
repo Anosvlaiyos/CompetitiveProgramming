6bdef65df078bfee43cdb4b5ea0782a3f9deb89f
Kevin Wan <kevin.wan59049@gmail.com>
2020-09-07 01:33:42 -0400

    Rewrote Dinic's template using adjacency list

--- dinics.cpp
+++ dinics.cpp
@@ -1,57 +1,64 @@
-#include<bits/stdc++.h>
-using namespace std;
-mt19937 rng(chrono::system_clock::now().time_since_epoch().count());
-mt19937_64 rngll(rng());
-typedef long long ll;
-typedef pair<int,int>pii;
-typedef pair<int,ll>pil;
-typedef pair<ll,ll>pll;
-const int mn=2e3+10;
-const ll mod=1e9+7;
-vector<int>g[mn];
-ll cap[mn][mn],ind[mn],dep[mn];
-const ll inf=0x3f3f3f3f3f3f3f3f;
-void add_edge(int a,int b,ll c){
-    g[a].push_back(b);
-    g[b].push_back(a);
-    cap[a][b]=c;
-}
-ll dfs(int x,int n,ll big){
-    if(n==x)return big;
-    for(;ind[x]<g[x].size();ind[x]++){
-        int y=g[x][ind[x]];
-        if(dep[y]!=dep[x]+1||cap[x][y]<=0)continue;
-        ll v=dfs(y,n,min(big,cap[x][y]));
-        if(v){
-            cap[x][y]-=v;
-            cap[y][x]+=v;
-            return v;
-        }
+template<typename T>
+struct dinic{
+    int n;
+    T inf;
+    struct E{
+        int to,rev;
+        T cap;
+    };
+    vector<vector<E>>g;
+    vector<int>at,dep;
+    dinic(int n,T inf=LLONG_MAX){
+        this->n=n;
+        this->inf=inf;
+        g.resize(n);
+        at.resize(n);
+        dep.resize(n);
     }
-    return 0;
-}
-bool bfs(int n){
-    memset(dep,0x3f,sizeof(dep));
-    memset(ind,0,sizeof(ind));
-    dep[0]=0;
-    queue<int>q;
-    q.push(0);
-    while(q.size()){
-        int x=q.front();
-        q.pop();
-        for(int y:g[x]){
-            if(dep[y]==inf&&cap[x][y]>0)dep[y]=dep[x]+1,q.push(y);
-        }
+    void addEdge(int x,int y,T c){
+        g[x].push_back({y,g[y].size(),c});
+        g[y].push_back({x,g[x].size()-1,0});
     }
-    return dep[n]!=inf;
-}
-ll maxflow(int n){
-    ll ans=0;
-    while(bfs(n)){
-        ll d=-1;
-        while(d){
-            ans+=(d=dfs(0,n,inf));
+    int s,t;
+    bool bfs(){
+        queue<int>q;
+        q.push(s);
+        fill(dep.begin(),dep.end(),-1);
+        dep[s]=0;
+        while(q.size()){
+            int x=q.front();
+            q.pop();
+            for(E e:g[x])if(dep[e.to]==-1&&e.cap>0){
+                dep[e.to]=dep[x]+1;
+                q.push(e.to);
+            }
         }
+        return dep[t]!=-1;
     }
-    return ans;
-}
+    T dfs(int x,T cap){
+        if(x==t)return cap;
+        for(;at[x]<g[x].size();at[x]++){
+            E e=g[x][at[x]];
+            if(e.cap<=0||dep[e.to]!=dep[x]+1)continue;
+            T v=dfs(e.to,min(cap,e.cap));
+            if(v){
+                g[x][at[x]].cap-=v;
+                g[e.to][e.rev].cap+=v;
+                return v;
+            }
+        }
+        return 0;
+    }
+    T maxflow(int s,int t){
+        this->s=s,this->t=t;
+        T ans=0;
+        while(bfs()){
+            fill(at.begin(),at.end(),0);
+            T v;
+            do{
+                ans+=(v=dfs(s,inf));
+            }while(v>0);
+        }
+        return ans;
+    }
+};
